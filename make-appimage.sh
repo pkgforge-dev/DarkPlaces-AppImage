51dd7553abcfb87812b2225ed38e389ad0884944
Link4Electronics <link4electronics@gmail.com>
2026-01-24 06:06:25 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q papermc | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q darkplaces-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/src//minecraft-server-1.0.4/paper-server/src/main/resources/logo.png
-export DESKTOP=DUMMY
+export ICON=/usr/share/icons/hicolor/72x72/apps/darkplaces.png
+export DESKTOP=/usr/share/applications/darkplaces-git.desktop
 
 # Deploy dependencies
-quick-sharun /usr/srv/papermc/papermc_server.jar
+quick-sharun /usr/bin/darkplaces-sdl
 
 # Additional changes can be done in between here
 
