2a0b2c1503f90ba3a510bbf66a92312fe91268df
Link4Electronics <link4electronics@gmail.com>
2026-01-24 06:13:50 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/72x72/apps/darkplaces.png
 export DESKTOP=/usr/share/applications/darkplaces-git.desktop
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/darkplaces-sdl
