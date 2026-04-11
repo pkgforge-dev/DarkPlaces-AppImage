e38ca13ab064ae6697307be224264f862daa3325
Link4Electronics <link4electronics@gmail.com>
2026-04-11 20:03:44 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,20 +3,19 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q darkplaces-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/72x72/apps/darkplaces.png
-export DESKTOP=/usr/share/applications/darkplaces-git.desktop
-export STARTUPWMCLASS=darkplaces-sdl
+export ICON=https://gitlab.com/xonotic/darkplaces/-/raw/master/darkplaces72x72.png?ref_type=heads
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/darkplaces-sdl
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/darkplaces-sdl
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
