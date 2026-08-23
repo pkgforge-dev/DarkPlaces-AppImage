a1bfe44bc8d19cb8fedff8f54131f275e5807114
Link4Electronics <link4electronics@gmail.com>
2026-08-23 20:18:19 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libdecor
+pacman -Syu --noconfirm sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-make-aur-package darkplaces-git
+echo "Building DarkPlaces..."
+echo "---------------------------------------------------------------"
+REPO="https://gitlab.com/xonotic/darkplaces"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./darkplaces
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin
+cd ./darkplaces
+make release -j$(nproc)
+mv -v darkplaces-dedicated darkplaces-sdl ../AppDir/bin
