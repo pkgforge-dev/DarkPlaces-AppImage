5b2d957439b61d9adaf7e9463d7087369134803c
Link4Electronics <link4electronics@gmail.com>
2026-08-26 15:59:59 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q darkplaces-git | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
@@ -14,7 +13,7 @@ export STARTUPWMCLASS=darkplaces-sdl
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/darkplaces-sdl
+quick-sharun ./AppDir/bin/darkplaces-sdl
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
