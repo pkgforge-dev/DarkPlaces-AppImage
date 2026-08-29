77755509a1aab0229e77e0cb8b94fb627e689b8b
Link4Electronics <link4electronics@gmail.com>
2026-08-29 20:42:47 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,6 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm sdl2-compat
-    
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
